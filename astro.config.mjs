b956d1ad4829eda450453d4913c36453f7c23514
David Eglin <david@astounding.agency>
2025-09-21 19:09:56 +0100

    try adding webmanifest, background color etc in an attempt to resolve viewport issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import webmanifest from "astro-webmanifest";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    webmanifest({
+      /**
+       * required
+       **/
+      name: "The Headless Company",
+
+      /**
+       * optional
+       **/
+      icon: "src/assets/favicon.svg", // source for favicon & icons
+
+      short_name: "HeadlessCo",
+      description: "Headless CMS migrations. Done right.",
+      start_url: "/",
+      theme_color: "#fe2c54",
+      background_color: "#fe2c54",
+      display: "fullscreen",
+    }),
+  ],
+});
