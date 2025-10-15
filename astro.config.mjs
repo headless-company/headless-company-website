edf0dd5de4ca2483d5d7581f6c142e81b60c72a2
David Eglin <david@astounding.agency>
2025-10-15 02:43:56 +0100

    add integrations for sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,27 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import webmanifest from "astro-webmanifest";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "astro-sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    webmanifest({
-      /**
-       * required
-       **/
-      name: "The Headless Company",
+  integrations: [webmanifest({
+    /**
+     * required
+     **/
+    name: "The Headless Company",
 
-      /**
-       * optional
-       **/
-      icon: "src/assets/favicon.svg", // source for favicon & icons
+    /**
+     * optional
+     **/
+    icon: "src/assets/favicon.svg", // source for favicon & icons
 
-      short_name: "HeadlessCo",
-      description: "Headless CMS migrations. Done right.",
-      start_url: "/",
-      theme_color: "#fe2c54",
-      background_color: "#fe2c54",
-      display: "fullscreen",
-    }),
-  ],
+    short_name: "HeadlessCo",
+    description: "Headless CMS migrations. Done right.",
+    start_url: "/",
+    theme_color: "#fe2c54",
+    background_color: "#fe2c54",
+    display: "fullscreen",
+  }), robotsTxt(), sitemap()],
 });
